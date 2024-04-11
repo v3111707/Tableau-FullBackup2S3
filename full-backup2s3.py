b6f208bf97a129501f219e656939b01aff898fcd
Sergey Korneev <s_korneev@wargaming.net>
2024-04-11 16:17:17 +0300

    m

--- full-backup2s3.py
+++ full-backup2s3.py
@@ -8,10 +8,11 @@ import os
 import configparser
 import boto3
 import socket
+from logging.handlers import TimedRotatingFileHandler
 
 LOGGER_NAME = 'main'
 CONFIG_FILE = 'config.ini'
-POSSIBLE_COMMANDS = ['backup', 'upload', 'zab-test']
+POSSIBLE_COMMANDS = ['backup', 'upload']
 HELP_MESSAGE = [
     'The full-backup2s3 is used to start "tsm maintenance backup", upload backups to the S3 and send result to Zabbix.',
     'It\'s uses only one non standard libary: boto3',
@@ -19,7 +20,6 @@ HELP_MESSAGE = [
     'Commands:',
     '   backup    --  Runs "tsm maintenance backup" and after completion starts the "upload" command.',
     '   upload    --  Upload all *.tsbak files in the backup folder to S3 and remove its',
-    '   zab-test  --  Sends in all items the "1" '
 ]
 
 
@@ -35,23 +35,48 @@ def init_logger(name: str = None, debug: bool = False, ) -> logging.Logger:
     return logger
 
 
+def init_filelogger(filename: str,
+                     when: str,
+                     interval: int,
+                     backup_count: int,
+                     name: str = None,
+                     debug: bool = False, ) -> None:
+    formatter = logging.Formatter('%(asctime)s: %(message)s')
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    filename_path = os.path.join(script_dir, filename)
+    backup_count = int(backup_count)
+    interval = int(interval)
+    handler = TimedRotatingFileHandler(filename=filename_path,
+                                       when=when,
+                                       interval=interval,
+                                       backupCount=backup_count)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+
+
 def get_timestamp() -> str:
     return time.strftime('%Y%m%d-%H%M%S')
 
 
 def get_config(path: str) -> configparser.ConfigParser:
+    logger = logging.getLogger(LOGGER_NAME)
     script_dir = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(script_dir, path)
-    logger = logging.getLogger(LOGGER_NAME)
     config = configparser.ConfigParser()
     logger.debug(f'Reading config file: {path}')
     if not os.path.isfile(config_path):
-        sys.exit(f'Config file not found: {path}')
+        msg = f'Config file not found: {path}. Exit'
+        logger.debug(msg)
+        sys.exit(msg)
     try:
         config.read(config_path)
     except Exception as e:
         logger.exception(e)
-        sys.exit(f'Exception while parsing the config file: {path}')
+        msg = f'Exception while parsing the config file: {path}'
+        logger.error(msg)
+        sys.exit(msg)
     return config
 
 
@@ -72,6 +97,7 @@ def send_to_zabbix(key: str, value: int, config_file: str) -> None:
         '-o',
         str(value)
     ]
+    logger.info(f'Send to Zabbix {key} - {value}')
     logger.debug(f'Running command: {" ".join(args)}')
     completed_process = subprocess.run(args)
 
@@ -106,6 +132,10 @@ def main():
     aws_conf = config['AWS']
     zab_conf = config['Zabbix'] if 'Zabbix' in config.sections() else None
 
+    if 'Logging' in config.sections():
+        init_filelogger(name=LOGGER_NAME, **config['Logging'])
+
+
     if len(sys.argv) == 1 or sys.argv[1] not in POSSIBLE_COMMANDS:
         print_help()
         sys.exit('\nMissing or unsupported command')
@@ -119,13 +149,6 @@ def main():
             append_timestamp = backup_conf.getboolean('append_timestamp'),
             multithreaded = backup_conf.getboolean('multithreaded')
         )
-
-        # # ToDo
-        # succsess_tmp = (0,
-        #                 "The previous GenerateBackupJob did not succeed after running for 6 minute(s).\nThe last successful run of GenerateBackupJob took 3 minute(s).\n\nJob id is '10', timeout is 1440 minutes.\n7% - Starting the Active Repository instance, File Store, and Cluster Controller.\nRunning - Waiting for the Active Repository, File Store, and Cluster Controller to start.\r                                                                                         \r14% - Waiting for the Active Repository, File Store, and Cluster Controller to start.\nRunning - Installing backup services.\r                                     \r21% - Installing backup services.\nRunning - Estimating required disk space.\r                                         \r28% - Estimating required disk space.\n35% - Gathering disk space information from all nodes.\n42% - Analyzing disk space information.\n50% - Checking if sufficient disk space is available on all nodes.\n57% - Backing up configuration.\nRunning - [Backing up object storage data.] [Backing up database.]\r                                                                  \r64% - Backing up object storage data.\nRunning - Backing up database.\r                              \r71% - Backing up database.\nRunning - Assembling the tsbak archive. Processing file 1 of 35.\r                                                                \r78% - Assembling the tsbak archive.\n85% - Stopping the Active Repository if necessary.\nRunning - Waiting for the Active Repository to stop if necessary.\r                                                                 \r92% - Waiting for the Active Repository to stop if necessary.\nRunning - Uninstalling backup services.\r                                       \r100% - Uninstalling backup services.\r                                    \r100% - Uninstalling backup services.\nBackup written to '/var/opt/tableau/tableau_server/data/tabsvc/files/backups/ts_backup-2024-04-08.tsbak' on the controller node.\n",
-        #                 '',
-        #                 69)
-        # tsm_return_code, tsm_stdout, tsm_stderr, tsm_backup_duration = succsess_tmp
 
         logger.debug(f'{tsm_stdout=},\n {tsm_stderr=},\n {tsm_backup_duration=},\n {tsm_return_code=}')
 
@@ -155,7 +178,7 @@ def main():
             if dir_entry.is_file() and dir_entry.name.endswith('.tsbak'):
                 backup_file_size = int(os.stat(dir_entry.path).st_size )
 
-                logger.info(f'Processing "{dir_entry.name}", size: {backup_file_size / (1024 * 1024)} MB')
+                logger.info(f'Processing file "{dir_entry.name}", size: {backup_file_size / (1024 * 1024)} MB')
                 process = subprocess.Popen([
                     'md5sum',
                     dir_entry.name,
@@ -196,7 +219,6 @@ def main():
                     send_to_zabbix(key='upload_duration',
                                    value=upload_duration,
                                    config_file=zab_conf['config_file'])
-
                 logger.info('Uploaded')
 
 
